1c302502440a25e2e73811cbe57c0103d89bbabc
Prajwal Sandeep S <prajwalsandeep@gmail.com>
2024-10-19 21:36:52 +0530

    port change to 3000

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ require('./app/routes/user.routes')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(3000, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
