de7d82a304efb7f4a5514bbedc74cdf08544f8a1
Prajwal Sandeep S <prajwalsandeep@gmail.com>
2024-11-11 21:23:25 +0530

    Changed filed name from username to name in seves model

--- app/models/seves.model.js
+++ app/models/seves.model.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, Sequelize) => {
     const Seves = sequelize.define("seves", {
-        username: {
+        name: {
             type: Sequelize.STRING
         },
         mobile: {
