3753ec54311ffc2e09f5bde90e2dbff96376b061
Prajwal Sandeep S <prajwalsandeep@gmail.com>
2024-11-14 20:20:51 +0530

    Changed seves table name to submitted_seves and changed seves table's schema to fit new logic for pre defined seves data to be stored

--- app/models/seves.model.js
+++ app/models/seves.model.js
@@ -3,21 +3,6 @@ module.exports = (sequelize, Sequelize) => {
         name: {
             type: Sequelize.STRING
         },
-        mobile: {
-            type: Sequelize.STRING
-        },
-        gothra: {
-            type: Sequelize.STRING
-        },
-        nakshatra: {
-            type: Sequelize.STRING
-        },
-        rashi: {
-            type: Sequelize.STRING
-        },
-        type: {
-            type: Sequelize.STRING
-        },
         amount: {
             type: Sequelize.STRING
         }
